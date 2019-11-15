aabe8b8deabe6e3655677c1a1f5e303201fd2a1a
Jeff Tyzzer <jtyzzer@talend.com>
2019-11-15 13:46:46 -0800

    Cascade triggers

--- libraryA/src/main/java/com/company/projectA/App.java
+++ libraryA/src/main/java/com/company/projectA/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins! Cascade Triggers" );
+        System.out.println( "Hello Jenkins! Cascade Triggers (2)" );
     }
 }
